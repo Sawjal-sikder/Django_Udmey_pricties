56570b6dbeccb8f0945f51c6ee008c4a91473680
Md Sawjal Sikder <134833280+Sawjal-sikder@users.noreply.github.com>
2024-07-12 03:03:54 +0600

    storing from data in a database

--- main/home/models.py
+++ main/home/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class personModel(models.Model):
+    first_name = models.CharField(max_length=55)
+    last_name = models.CharField(max_length=55)
+
+    def __str__(self):
+        return f'{self.first_name}'
--- main/home/views.py
+++ main/home/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from .form import person
-
+from .models import personModel
 
 # Create your views here.
 def home(request):
@@ -8,7 +8,10 @@ def home(request):
         form = person(request.POST)
 
         if form.is_valid():
-            print(form.cleaned_data)
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            Personalinfo = personModel(first_name=first_name,last_name=last_name)
+            Personalinfo.save()
             return redirect('success')
     else:
         form = person
