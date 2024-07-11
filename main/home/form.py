c8430a5d0d1add4419ae89bb4d43660385d75edd
Md Sawjal Sikder <134833280+Sawjal-sikder@users.noreply.github.com>
2024-07-12 02:05:45 +0600

    customization the form control

--- main/home/form.py
+++ main/home/form.py
@@ -1,5 +1,19 @@
 from django import forms
 
 class person(forms.Form):
-    first_name = forms.CharField(max_length=55)
-    last_name = forms.CharField(max_length=55)
+    first_name = forms.CharField(
+        label="Your First Name",
+        max_length=55,
+        error_messages={
+            "required": "First name must not be empty!",
+            "max_length": "Please shorten your first name."
+        }
+    )
+    last_name = forms.CharField(
+        label="Your Last Name",
+        max_length=55,
+        error_messages={
+            "required": "Last name must not be empty!",
+            "max_length": "Please shorten your last name."
+        }
+    )
